2a4b7e02a3725be254759276156a98750a69af4c
David Moreno <damomar73@gmail.com>
2022-11-01 22:16:26 +0100

    Update retos faciles

--- Python/Basicos/Reto13.py
+++ Python/Basicos/Reto13.py
@@ -1,8 +1,19 @@
 #reto 13 - NIVEL FACIL
+
+#importamos el numero pi de la libreria math
 from math import pi
-def areaT(baseT, alturaT):
+
+def areaT():
+  baseT = float(input("> Introduce la base del triangulo: "))
+  alturaT = float(input("> Introduce la altura del triangulo: "))
   areaTri = (baseT * alturaT / 2)
   print (f'> El area del triangulo es:  {areaTri} cm2')
-def areaC (radio: float) -> float:
-  areaCir = float (pi * (radio ** 2))
+
+def areaC():
+  radio = float(input("> Introduce el radio del circulo: "))
+  areaCir = pi * (radio ** 2)
   print (f'> El area del circulo es:  {areaCir} cm2')
+
+if __name__ == "__main__":   
+  areaT()
+  areaC()
--- Python/Basicos/Reto14.py
+++ Python/Basicos/Reto14.py
@@ -1,12 +1,19 @@
 #reto 14 - NIVEL FACIL
+
+#importamos el numero pi de la libreria math
 from math import pi
 
 def areaC():
   radio = float (input ("> Introduce el radio del cilindro (cm): "))
   global areaCir
-  areaCir = float (pi * (radio ** 2))
+  areaCir = pi * (radio ** 2)
   print (f'> El area del circulo es:  {areaCir} cm2')
 
-def volumenC(alturaC: float)-> float:
+def volumenC():
+  alturaC = float(input("> Introduce la altura del cilindro: "))
   volumenC = areaCir * alturaC
   print (f'> El volumen del cilindro es: {volumenC} cm2')
+
+if __name__ == "__main__":
+  areaC()
+  volumenC()
