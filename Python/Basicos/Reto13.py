cc4496257eb6906c6fa4a8c7993e9571b78dd328
David Moreno <damomar73@gmail.com>
2022-11-02 16:14:52 +0100

    Actualizacion reto 13 facil

--- Python/Basicos/Reto13.py
+++ Python/Basicos/Reto13.py
@@ -1,19 +1,23 @@
 #reto 13 - NIVEL FACIL
 
-#importamos el numero pi de la libreria math
+# importamos el nº pi de la libreria math
 from math import pi
 
-def areaT():
-  baseT = float(input("> Introduce la base del triangulo: "))
-  alturaT = float(input("> Introduce la altura del triangulo: "))
-  areaTri = (baseT * alturaT / 2)
+#introducimos los datos desde consola
+baseT = float(input('> Introduce la base del circulo: '))
+alturaT = float(input('> Introduce la altura del circulo: '))
+radio = float(input('> Introduce el radio del circulo: '))
+
+#definimos la funcion de calculo de area del triangulo
+def areaT(baseT, alturaT):
+  areaTri = baseT * alturaT / 2
   print (f'> El area del triangulo es:  {areaTri} cm2')
 
-def areaC():
-  radio = float(input("> Introduce el radio del circulo: "))
+#definimos la funcion de calculo del area del circulo
+def areaC (radio):
   areaCir = pi * (radio ** 2)
   print (f'> El area del circulo es:  {areaCir} cm2')
 
-if __name__ == "__main__":   
-  areaT()
-  areaC()
+if __name__ == "__main__":
+  areaT(baseT, alturaT)
+  areaC(radio)
